dbbe2cf6e9431f02fc0c7c65b0cbb3d2e51063fc
Manish Tiwari <manish.t92@gmail.com>
2021-08-25 00:37:33 +0530

    Navigation Event Binding

--- AngularDemo/src/app/app.component.ts
+++ AngularDemo/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularDemo';
+
+  loadedFeature: string = 'recipe';
+  onNavigate(feature_name: string) {
+    this.loadedFeature = feature_name;
+  }
 }
